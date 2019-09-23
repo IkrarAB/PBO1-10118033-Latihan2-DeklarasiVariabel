aee89c582d49ffc339cfd38d338f55da1fd47987
IkrarAB <ikraranugrah26@gmail.com>
2019-09-23 22:22:03 +0700

    Initial commit

---
+++ src/pbo1/pkg10118033/latihan2/PBO110118033Latihan2.java
@@ -0,0 +1,29 @@
+package pbo1.pkg10118033.latihan2;
+
+/*
+ *
+ * @author
+ * NAMA  : Ikrar Anugrah Bastary
+ * KELAS : PBO1
+ * NIM   : 10118033
+ *
+ */
+
+public class PBO110118033Latihan2 {
+    public static void main(String[] args) {
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+    }
+}
